72ebaf01805adc705bbad26f59c0410f87480a54
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-05 00:37:31 -0300

    Adição das configurações do firebase para o modo de desenvolvimento.

---
+++ src/app/environment/app.environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDHILN5844hcSrsNbepmkjtZlWA4nvN_Uk",
+  authDomain: "tawk-hawk-dev.firebaseapp.com",
+  databaseURL: "https://tawk-hawk-dev.firebaseio.com",
+  projectId: "tawk-hawk-dev",
+  storageBucket: "tawk-hawk-dev.appspot.com",
+  messagingSenderId: "686129459384"
+}
