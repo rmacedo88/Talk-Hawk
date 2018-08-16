584e0637ddd290e1fbd0cc9f42637ed95cad63b0
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-16 18:48:26 -0300

    Módulo para carregamento lazy das diretivas. A diretiva em questão está em vias de ser desativada em virtude do redesign da aplicação.

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AsyncImgLoaderDirective } from './async-img-loader/async-img-loader';
+@NgModule({
+  declarations: [AsyncImgLoaderDirective],
+  imports: [],
+  exports: [AsyncImgLoaderDirective]
+})
+export class DirectivesModule { }
