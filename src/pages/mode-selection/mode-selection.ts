c52aa66fd58a9c87cdca6fb0bb9fcfa3d23a9e85
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-16 21:34:56 -0300

    Implementação do componente Call to Action e seus respectivos eventos

--- src/pages/mode-selection/mode-selection.ts
+++ src/pages/mode-selection/mode-selection.ts
@@ -1,7 +1,16 @@
+import { concat } from 'rxjs/operators/concat';
+import { merge } from 'rxjs/operators/merge';
+import { interval } from 'rxjs/observable/interval';
+import { switchMap } from 'rxjs/operators/switchMap';
+import { timer } from 'rxjs/observable/timer';
+import { ChooseExamLevelPage } from './../choose-exam-level/choose-exam-level';
 import { CareerModePage } from './../career-mode/career-mode';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ChooseExamPage } from '../choose-exam/choose-exam';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Navbar } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { take } from 'rxjs/operators/take';
+import { map } from 'rxjs/operators/map';
 
 /**
  * Generated class for the ModeSelectionPage page.
@@ -17,24 +26,54 @@ import { ChooseExamPage } from '../choose-exam/choose-exam';
 })
 export class ModeSelectionPage {
 
+  @ViewChild(Navbar) navBar: Navbar;
+
+  actions: Array<any> = [
+    { iconName: 'exam', label: 'Modo Exame' },
+    { iconName: 'career', label: 'Modo Carreira' },
+    { iconName: 'vocabulary', label: 'Vocabulário' },
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // this.navCtrl.pop({ animate: false });
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ModeSelectionPage');
+    this.navBar.backButtonClick = (e: UIEvent) => {
+      this.navCtrl.pop({ animate: false });
+    }
   }
 
-  toBack() {
-    this.navCtrl.pop({ animate: false });
+  ionViewDidEnter() {
+
   }
 
-  goToChooseExam() {
-    this.navCtrl.push(ChooseExamPage, {}, { animate: false });
+  goToDesiredLevel($event) {
+    switch ($event) {
+
+      case 'exam':
+        this.navCtrl.push(ChooseExamLevelPage, {}, { animate: false });
+        break;
+
+      case 'career':
+        this.navCtrl.push(CareerModePage, {}, { animate: false });
+        break;
+
+      case 'vocabulary':
+        this.navCtrl.push(CareerModePage, {}, { animate: false });
+        break;
+
+      default:
+        break;
+    }
   }
 
-  goToCareerMode() {
-    this.navCtrl.push(CareerModePage, {}, { animate: false });
-  }
+  // goToChooseExam() {
+  //   this.navCtrl.push(ChooseExamLevelPage, {}, { animate: false });
+  // }
+
+  // goToCareerMode() {
+  //   this.navCtrl.push(CareerModePage, {}, { animate: false });
+  // }
 
 }
