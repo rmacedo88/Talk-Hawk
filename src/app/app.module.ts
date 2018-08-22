9cce3e6ea5f92830d6a1fb0b1252640b335b0825
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-22 11:03:55 -0300

    Mapeamento do provider responsável por invocar a API talk hawk.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { firebaseConfig } from './environment/app.environment.prod';
+import { firebaseConfig } from './environment/environment';
 import { MODULES, PROVIDERS, DIRECTIVES } from './app.resources';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -10,6 +10,7 @@ import { HomePage } from '../pages/home/home';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AuthProvider } from '../providers/auth/auth';
+import { TalkHawkApiProvider } from '../providers/talk-hawk-api/talk-hawk-api';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { AuthProvider } from '../providers/auth/auth';
     PROVIDERS,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     AuthProvider,
+    TalkHawkApiProvider,
   ]
 })
 export class AppModule { }
