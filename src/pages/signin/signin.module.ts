5e71b398b4b3a4fea1529b8ce071edf612635880
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-22 16:04:43 -0300

    adição dos módulos lazy loaded

--- src/pages/signin/signin.module.ts
+++ src/pages/signin/signin.module.ts
@@ -1,3 +1,5 @@
+import { ComponentsModule } from './../../components/components.module';
+import { DirectivesModule } from './../../directives/directives.module';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SigninPage } from './signin';
@@ -7,7 +9,9 @@ import { SigninPage } from './signin';
     SigninPage,
   ],
   imports: [
+    ComponentsModule,
+    DirectivesModule,
     IonicPageModule.forChild(SigninPage),
   ],
 })
-export class SigninPageModule {}
+export class SigninPageModule { }
