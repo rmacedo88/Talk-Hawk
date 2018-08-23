c56915f904878598929685e7e38a1a07eec2668f
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-23 18:57:08 -0300

    Refatoração para adição de comentários relevantes

--- src/pages/exam-mode/exam-mode.ts
+++ src/pages/exam-mode/exam-mode.ts
@@ -1,3 +1,4 @@
+import { ExamQuestionComponent } from './../../components/exam-question/exam-question';
 import { ChooseExamLevelPage } from './../choose-exam-level/choose-exam-level';
 import { TalkHawkApiProvider } from './../../providers/talk-hawk-api/talk-hawk-api';
 import { Component } from '@angular/core';
@@ -11,42 +12,36 @@ import { timer } from 'rxjs/observable/timer';
 })
 export class ExamModePage {
 
+  // Nível de dificuldade escolhido
   examLevel: string;
+  // Pontuação conquistada nessa rodada
   pointsEarned: number = 0;
 
+  // Informações referentes à questões recuperadas do firebase
   private _questionsRawData;
 
+  // Objeto exibido na tela com os dados da questão atual
   currentQuestionObject: any;
-  //  {
-  //   title: string,
-  //   options: [
-  //     { iconName: 'a', label: null, uid: null },
-  //     { iconName: 'b', label: null, uid: null },
-  //     { iconName: 'c', label: null, uid: null },
-  //     { iconName: 'd', label: null, uid: null }
-  //   ]
-  // }
 
-  // questionTitle;
-
+  // Índice que identifica a próxima questão a ser exibida para o usuário
   currentQuestion: number = 1;
-  totalQuestions: number = 0;
 
-  // actions: Array<any> = [
-  //   { iconName: 'a', label: null },
-  //   { iconName: 'b', label: null },
-  //   { iconName: 'c', label: null },
-  //   { iconName: 'd', label: null }
-  // ];
+  // Total de questões para essa rodada
+  totalQuestions: number = 0;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private talkHawkApi: TalkHawkApiProvider
   ) {
+    // [Recupera o a dificuldade escolhida pelo usuário]
     this.examLevel = this.navParams.get('level');
   }
 
+  /**
+   * Recupera a lista de questões usando a api talk hawk.
+   * Determina o número de questões para essa rodada e carrega os dados da primeira questão na tela.
+   */
   async ionViewDidLoad() {
     try {
       // [Invoca a API talk hawk e obtém todas as quest!oes de um determinado nível (Fácil ou Difícil)]
@@ -64,8 +59,13 @@ export class ExamModePage {
     //   .catch(err => console.log(err));
   }
 
+  /**
+   * Carrega os dados de uma questão na tela.
+   * Esse carregamento respeita o índice 'currentQuestion'
+   */
   createQuestion() {
 
+    // [Formato do objeto que representa uma questão]
     this.currentQuestionObject = {
       title: null,
       options: [
@@ -76,27 +76,30 @@ export class ExamModePage {
       ]
     };
 
-    this.currentQuestionObject.title = this._questionsRawData[this.currentQuestion - 1].text;
     // [Recupera o enunciado da próxima pergunta]
-    // this.questionTitle = this._questionsRawData[this.currentQuestion - 1].text;
+    this.currentQuestionObject.title = this._questionsRawData[this.currentQuestion - 1].text;
+
     // [Recupera as opções de resposta junto ao id da questão]
     this.currentQuestionObject.options.forEach((item, index) => {
       item.label = this._questionsRawData[this.currentQuestion - 1].options[index];
       item.uid = this._questionsRawData[this.currentQuestion - 1].uid;
     });
-    // this.actions.forEach((item, index) => {
-    //   item.label = this._questionsRawData[this.currentQuestion - 1].options[index];
-    //   item.uid = this._questionsRawData[this.currentQuestion - 1].uid;
-    // });
 
-    console.log(this.currentQuestionObject);
   }
 
+  /**
+   * Avança o índice em uma posição e carrega a nova questão
+   */
   goToNextQuestion() {
     this.currentQuestion++;
     this.createQuestion();
   }
 
+  /**
+   * Efetua a avaliação da resposta do usuário a cada questão respondida
+   * @param responseOption uma letra entre 'a' e 'd'
+   * @param id UID de um determinado documento da collection 'questions'
+   */
   async questionResponse(responseOption: string, id: string) {
 
     // [Envia a resposta para avaliação]
@@ -110,12 +113,15 @@ export class ExamModePage {
     // [Recupera o feedback e incrementa a pontuação do usuário]
     this.pointsEarned += (responseFeedback.points) ? responseFeedback.points : 0;
 
+    // [Checa se essa resposta se refere à última questão]
     if (this.currentQuestion === this.totalQuestions) {
+      // [SERÁ ATUALIZADO: Atualmente envia o usuário de volta para a tela de escolha da dificuldade]
       timer(2000)
         .subscribe(() => {
           this.navCtrl.setRoot(ChooseExamLevelPage);
         });
     } else {
+      // [Se essa não é a última resposta, avança para a próxima questão]
       this.goToNextQuestion();
     }
 
