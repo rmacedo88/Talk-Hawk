a0a421da9e7bb86ddabeff27638f46a8b375f064
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-23 14:37:34 -0300

    Adição de um loader, que será exibido sempre que uma solicitação http ocorrer.

--- src/providers/talk-hawk-api/talk-hawk-api.ts
+++ src/providers/talk-hawk-api/talk-hawk-api.ts
@@ -4,36 +4,70 @@ import { of } from 'rxjs/observable/of';
 import { switchMap } from 'rxjs/operators/switchMap';
 import { map } from 'rxjs/operators/map';
 import { environment } from '../../app/environment/environment';
+import { LoadingController, Loading } from 'ionic-angular';
 
 @Injectable()
 export class TalkHawkApiProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(
+    public http: HttpClient,
+    private loadingController: LoadingController
+  ) {
   }
 
-  async get(endpoint: string, params?: Array<any>): Promise<any> {
+  private _loader: Loading;
+
+  private showLoader() {
+    this._loader = this.loadingController.create({
+      spinner: "hide",
+      content: `
+          <div class="custom-spinner-container">
+          <div class="spinner">
+            <div class="double-bounce1"></div>
+            <div class="double-bounce2"></div>
+          </div>
+          </div>`
+    });
+    this._loader.present();
+  }
+
+  private hideLoader() {
+    this._loader.dismiss();
+  }
+
+  async get(endpoint: string): Promise<any> {
+    this.showLoader();
     return await of([''])
       .pipe(
         map(() => `${environment.API_URL}${endpoint}`),
-        switchMap(url => this.http.get(url))
+        switchMap(url => this.http.get(url)),
+        map(response => {
+          this.hideLoader();
+          return response;
+        })
       )
       .toPromise();
   }
 
-  async put(endpoint: string, params?: any): Promise<any> {
+  async put(endpoint: string, body?: any): Promise<any> {
     return await of([''])
       .pipe(
         map(() => `${environment.API_URL}${endpoint}`),
-        switchMap(url => this.http.put(url, params || null))
+        switchMap(url => this.http.put(url, body || null))
       )
       .toPromise();
   }
 
-  async post(endpoint: string, params?: any): Promise<any> {
+  async post(endpoint: string, body?: any): Promise<any> {
+    this.showLoader();
     return await of([''])
       .pipe(
         map(() => `${environment.API_URL}${endpoint}`),
-        switchMap(url => this.http.post(url, params || null))
+        switchMap(url => this.http.post(url, body || null)),
+        map(response => {
+          this.hideLoader();
+          return response;
+        })
       )
       .toPromise();
   }
