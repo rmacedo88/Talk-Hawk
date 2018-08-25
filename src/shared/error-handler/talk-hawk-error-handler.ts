b19ef8b7019ba14ed74dbbd0e84a1e2824482c25
rmacedo88 <robson.alves.macedo@gmail.com>
2018-08-25 14:16:32 -0300

    Error Handler customizado. Mostra as mensagens de erro tratadas na forma de um toast vermelho posicionado no topo da tela.

---
+++ src/shared/error-handler/talk-hawk-error-handler.ts
@@ -0,0 +1,49 @@
+import { ErrorHandler, Injector, Injectable, Inject } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+
+@Injectable()
+export class TalkHawkErrorHandler extends ErrorHandler {
+
+
+  /**
+   *
+   * @param injector O Angular não suporta a injeção de dependência convencional.
+   * O injetor habilita a injeção de dependência.
+   */
+  constructor(@Inject(Injector) private injector: Injector) {
+    super();
+  }
+
+
+  /**
+   * Injeta o ToastController e retorna uma instância dele
+   */
+  private get toastProvider(): ToastController {
+    return this.injector.get(ToastController);
+  }
+
+
+  /**
+   * Recupera uma instância do ToastController e mostra as
+   *  mensagens de erro na forma de toasts.
+   * @param error Mensagem de erro
+   */
+  handleError(error: any): void {
+
+    // [Mostra o erro na forma de um toast]
+    this.toastProvider
+      .create({
+        message: error,
+        duration: 4000,
+        position: 'top',
+        showCloseButton: true,
+        closeButtonText: 'fechar',
+        cssClass: 'toast-error'
+      })
+      .present();
+
+    // [Trata o erro da maneira padrão (exibindo no console de erros)]
+    super.handleError(error);
+  }
+
+}
